7454fc89faa57af46748b4911ea51d3aad1a8cae
Shaikh Sameer <Shaikh Sameer@LAPTOP-RL8EV5DL>
2024-01-05 21:06:00 +0530

    Code Is written

--- Demo Prj1/Program.cs
+++ Demo Prj1/Program.cs
@@ -11,7 +11,36 @@ namespace Demo_Prj1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Wwlcomw to github"); //to display out 
+            // Prompt the user to enter the height of the pyramid
+            Console.Write("Enter the height of the pyramid: ");
+
+            if (int.TryParse(Console.ReadLine(), out int height))
+            {
+                // Call the method to generate and display the pyramid
+                GeneratePyramid(height);
+            }
+            else
+            {
+                // Display an error message if the input is not a valid integer
+                Console.WriteLine("Please enter a valid integer for the height.");
+            }
+            Console.ReadLine();
         }
+        static void GeneratePyramid(int num)
+        {
+            int numberoflayer = num, Space, Number;
+            Console.WriteLine("Print paramid");
+            for (int i = 1; i <= numberoflayer; i++) // Total number of layer for pramid  
+            {
+                for (Space = 1; Space <= (numberoflayer - i); Space++) // Loop For Space  
+                    Console.Write(" ");
+                for (Number = 1; Number <= i; Number++) //increase the value  
+                    Console.Write(Number);
+                for (Number = (i - 1); Number >= 1; Number--) //decrease the value  
+                    Console.Write(Number);
+                Console.WriteLine();
+            }
+        }
+
     }
 }
